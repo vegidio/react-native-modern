8351d3d55e169df2ef049f3d8edff3572663bf08
Vinicius Egidio <me@vinicius.io>
2020-11-26 15:58:47 +0100

    Added error handling with await-to-js

---
+++ app/services/index.ts
@@ -0,0 +1,3 @@
+import GithubService from './GithubService';
+
+export { GithubService };
--- app/views/user/UserScreen.tsx
+++ app/views/user/UserScreen.tsx
@@ -1,15 +1,20 @@
 import React, { FunctionComponent, useEffect } from 'react';
 import { Text } from 'react-native';
-import GithubService from '../../services/GithubService';
+import to from 'await-to-js';
+import { GithubService } from '../../services';
 
 const UserScreen: FunctionComponent = () => {
     const service = new GithubService();
 
     useEffect(() => {
-        console.log('Entrou');
-
         const fetchData = async () => {
-            const user = await service.getUser('vegidio');
+            let [error, user] = await to(service.getUser('vegidioxxxxx'));
+
+            if (error) {
+                console.error(error);
+                return;
+            }
+
             console.log(user);
         };
 
