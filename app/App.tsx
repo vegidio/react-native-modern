c1b596537f74d6843a142f41d2c065fea453b404
Vinicius Egidio <me@vinicius.io>
2020-11-30 18:12:52 +0100

    Changed the background of React Navigation

--- app/App.tsx
+++ app/App.tsx
@@ -8,7 +8,7 @@ const Stack = createStackNavigator();
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
+            <Stack.Navigator initialRouteName="Home" screenOptions={{ cardStyle: { backgroundColor: 'white' } }}>
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="User" component={UserScreen} />
             </Stack.Navigator>
